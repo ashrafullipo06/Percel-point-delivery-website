7ddf41e9d6ef2542cfd42729fac1062fcb441dd2
Ashraful Islam <ashrafulislamlip06@gmail.com>
2025-01-13 19:31:29 +0600

    first commit

---
+++ src/hooks/useAxiosPublic.jsx
@@ -0,0 +1,9 @@
+const axiosPublic = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+const useAxiosPublic = () => {
+  return axiosPublic;
+};
+
+export default useAxiosPublic;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import Router from "./routes/Router";
+import AuthProvider from "./providers/AuthProvider";
+import { HelmetProvider } from "react-helmet-async";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={Router} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </AuthProvider>
+  </StrictMode>
+);
