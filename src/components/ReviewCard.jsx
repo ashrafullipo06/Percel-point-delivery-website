9c16257c500f06e686ae59eccfe5b0c5603a8a21
Ashraful Islam <ashrafulislamlip06@gmail.com>
2025-01-17 12:04:06 +0600

    Review Show

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,16 +1,37 @@
-const ReviewCard = ({ img, name, deliverCount, avgRating }) => {
+const ReviewCard = ({ review }) => {
+  const { deliverManName, delivermanPhoto, feedBack, rating } = review;
+
   return (
-    <div className="py-8">
-      <div
-        className="bg-red-500 shadow-lg rounded-lg p-4 max-w-sm mx-auto relative py-24
-         mt-8"
-      >
+    <div className=" rounded-lg p-6 mx-auto  w-full max-w-md relative mt-24 bg-slate-100 ">
+      {/* Delivery Man Photo */}
+      <div className="relative -mt-20 flex justify-center">
         <img
-          className="w-24 h-24 rounded-full mx-auto border-2 border-blue-500 object-cover absolute inset-0 -top-10"
-          src={img}
-          alt={name}
+          src={delivermanPhoto}
+          alt={`${deliverManName}'s photo`}
+          className="w-24 h-24 rounded-full object-cover border-4 border-white shadow-md"
         />
-        <h2 className="text-3xl font-semibold text-center mt-2">{name}</h2>
+      </div>
+
+      {/* Delivery Man Name */}
+      <div className="text-center mt-4">
+        <h3 className="text-xl font-semibold text-gray-800">
+          {deliverManName}
+        </h3>
+      </div>
+
+      {/* Feedback */}
+      <p className="mt-4 text-gray-600 text-sm leading-relaxed text-center">
+        "{feedBack}"
+      </p>
+
+      {/* Rating */}
+      <div className="mt-4 flex justify-center items-center">
+        <div className="text-yellow-500 text-lg">
+          {Array.from({ length: rating }, (_, i) => (
+            <span key={i}>&#9733;</span>
+          ))}
+        </div>
+        <span className="text-gray-500 text-sm ml-2">({rating} / 5)</span>
       </div>
     </div>
   );
--- src/pages/Home/TopRatedDeliveryMan/TopRatedDeliveryMan.jsx
+++ src/pages/Home/TopRatedDeliveryMan/TopRatedDeliveryMan.jsx
@@ -1,14 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
 import Heading from "../../../components/Heading";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import ReviewCard from "../../../components/ReviewCard";
 
 const TopRatedDeliveryMan = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data: reviews = [], isLoading } = useQuery({
+    queryKey: ["ratings"],
+    queryFn: async () => {
+      const res = await axiosPublic("/ratings");
+      return res.data;
+    },
+  });
+  console.log(isLoading);
   return (
     <div>
       <Heading title="Top Rated Delivery Men" />
+
       <div className="grid grid-cols-3 gap-8">
-        <ReviewCard />
-        <ReviewCard />
-        <ReviewCard />
+        {reviews?.map((review) => (
+          <ReviewCard key={review._id} review={review} />
+        ))}
       </div>
     </div>
   );
